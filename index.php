2203ca56d53bb79fe58ae766026fe75ab09b486c
fritz <jiezzing@gmail.com>
2021-02-18 18:24:24 +0800

    proweaver hands on exam

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+
+	<div id="content">
+		<div class="topnav float-center">
+		  <a class="active" href="#home">HOME</a>
+		  <a class="active" href="#home">ABOUT US</a>
+		  <a class="active" href="#home">SERVICES</a>
+		  <a class="active" href="#home">CAREERS</a>
+		  <a class="active" href="#home">FLEET/VEHICLES</a>
+		  <a class="active" href="#home">CONTACT US</a>
+		</div>
+		<div>
+		  <img src="images/transport.png" width="100%">
+		</div>
+		<div class="service-container">
+			<div class="service-content">
+				<div>
+					<div class="float-center">
+				  		<h1>OUR SERVICES</h1>
+					</div>
+					<div class="text-center">
+				  		<p>Additional business details will be uploaded on this website soon. <br> You are reading dummy text as placeholders for this layout</p>
+				  	</div>
+				</div>
+				<div class="float-center services">	
+					<figure>
+				  		<img class="center-image" src="images/tracking.png" width="75%">
+					  	<figcaption class="float-center font-bold fig-title">TRACKING</figcaption>
+					  	<figcaption>
+					  		<p class="text-center">Additional business details <br> will be uploaded on this <br> website soon.</p>
+					  	</figcaption>
+					  	<div class="float-center">
+					  		<button>Click Here</button>
+					  	</div>
+					</figure>
+					<figure>
+				  		<img class="center-image" src="images/warehouse.png" width="75%">
+					  	<figcaption class="float-center font-bold fig-title">WAREHOUSING</figcaption>
+					  	<figcaption>
+					  		<p class="text-center">Additional business details <br> will be uploaded on this <br> website soon.</p>
+					  	</figcaption>
+					  	<div class="float-center">
+					  		<button>Click Here</button>
+					  	</div>
+					</figure>
+					<figure>
+				  		<img class="center-image" src="images/specialty-services.png" width="75%">
+					  	<figcaption class="float-center font-bold fig-title">SPECIALTY SERVICES</figcaption>
+					  	<figcaption>
+					  		<p class="text-center">Additional business details <br> will be uploaded on this <br> website soon.</p>
+					  	</figcaption>
+					  	<div class="float-center">
+					  		<button>Click Here</button>
+					  	</div>
+					</figure>
+					<figure>
+				  		<img class="center-image" src="images/view-more.png" width="75%">
+					  	<figcaption class="float-center font-bold fig-title">VIEW MORE SERVICES</figcaption>
+					  	<figcaption>
+					  		<p class="text-center">Additional business details <br> will be uploaded on this <br> website soon.</p>
+					  	</figcaption>
+					  	<div class="float-center">
+					  		<button>Click Here</button>
+					  	</div>
+					</figure>
+				</div>
+			</div>
+		</div>
+		<div class="transport-container" style="background-image: url(images/trucks.jpg)">
+			<div class="services">	
+				<div class="transport-content">
+			  		<h2>Welcome to</h2>
+			  		<h1>PIONEER <br> TRANSPORT INC.</h1>
+			  		<p>Contents are for display purposes only.  This space is reserved for more details about the company. Additional business details will be uploaded on this website soon.</p>
+			  		<p>Contents are for display purposes only.  This space is reserved for more details about the company.</p>
+				</div>
+			</div>
+		</div>
+		<div>
+		  <img src="images/trucks.jpg" width="100%">
+		</div>
+	</div>
+</body>
+</html>
